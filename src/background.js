09eb7a1796d8ec5aaad1155f8273ff59170e8f35
huangdl <@huangdl>
2021-06-23 17:36:47 +0800

    增加更新代码

--- src/background.js
+++ src/background.js
@@ -74,7 +74,7 @@ function createWindow() {
   });
 
   notification = new Notification();
-
+  // 窗口加载完毕
   win.webContents.on("did-finish-load", () => {
     // 接收store中的数据
     win.webContents.send("electron-store-get", store.get("store"));
@@ -84,6 +84,7 @@ function createWindow() {
 // 检测并强制关闭
 function setClose() {
   win.close();
+  app.quit();
   setTimeout(() => {
     if (!win.isDestroyed()) {
       dialog
@@ -230,24 +231,82 @@ app.on("ready", async () => {
     win.webContents.reload();
   });
   createWindow();
+  checkUpdate();
 });
 
-autoUpdater.on("checking-for-update", () => {});
-autoUpdater.on("update-available", info => {
-  console.log(info);
-  dialog.showMessageBox({
-    title: "新版本发布",
-    message: "有新内容更新，稍后将重新为您安装",
-    buttons: ["确定"],
-    type: "info",
-    noLink: true
+const checkUpdate = win => {
+  //处理更新操作
+  const returnData = {
+    error: {
+      status: -1,
+      msg: "更新时发生意外，无法进行正常更新！"
+    },
+    checking: {
+      status: 0,
+      msg: "正在检查更新……"
+    },
+    updateAva: {
+      status: 1,
+      msg: "正在升级……"
+    },
+    updateNotAva: {
+      status: 2,
+      msg: "开始加载程序……"
+    }
+  };
+
+  //更新错误事件
+  autoUpdater.on("error", function(error) {
+    sendUpdateMessage(returnData.error);
+    log.info(returnData.error, error);
   });
-});
 
-autoUpdater.on("update-downloaded", info => {
-  console.log(info);
-  autoUpdater.quitAndInstall();
-});
+  //检查事件
+  autoUpdater.on("checking-for-update", function() {
+    sendUpdateMessage(returnData.checking);
+    log.info(returnData.checking);
+  });
+
+  //发现新版本
+  autoUpdater.on("update-available", function() {
+    // dialog.showMessageBox({
+    //   title: "新版本发布",
+    //   message: "有新内容更新，稍后将重新为您安装",
+    //   buttons: ["确定"],
+    //   type: "info",
+    //   noLink: true
+    // });
+    sendUpdateMessage(returnData.updateAva);
+    log.info(returnData.updateAva);
+  });
+
+  //当前版本为最新版本
+  autoUpdater.on("update-not-available", function() {
+    setTimeout(function() {
+      sendUpdateMessage(returnData.updateNotAva);
+      log.info(returnData.updateNotAva);
+    }, 1000);
+  });
+
+  //更新下载进度事件
+  autoUpdater.on("download-progress", function(progressObj) {
+    win.webContents.send("downloadProgress", progressObj);
+    log.info("正在下载", progressObj);
+  });
+
+  //下载完毕
+  // autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+  autoUpdater.on("update-downloaded", function() {
+    //退出并进行安装（这里可以做成让用户确认后再调用）
+    autoUpdater.quitAndInstall();
+    log.info("下载完毕");
+  });
+
+  //发送消息给窗口
+  function sendUpdateMessage(text) {
+    win.webContents.send("message", text);
+  }
+};
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
--- vue.config.js
+++ vue.config.js
@@ -6,12 +6,13 @@ module.exports = {
       builderOptions: {
         appId: "", //appid
         productName: "优享音乐", //软件名称
+        publish: ["github"],
         win: {
-          icon: "",
+          icon: ""
         },
         mac: {
           target: "dmg",
-          icon: "",
+          icon: ""
         },
         dmg: {
           title: "", //安装包名
@@ -19,26 +20,26 @@ module.exports = {
           contents: [
             {
               x: 110,
-              y: 150,
+              y: 150
             },
             {
               x: 240,
               y: 150,
               type: "link",
-              path: "/Applications",
-            },
+              path: "/Applications"
+            }
           ],
           window: {
             x: 400,
-            y: 400,
-          },
+            y: 400
+          }
         },
         nsis: {
           oneClick: false, // 是否一键安装，建议为 false，可以让用户点击下一步、下一步、下一步的形式安装程序，如果为true，当用户双击构建好的程序，自动安装程序并打开，即：一键安装
-          allowToChangeInstallationDirectory: true, // 允许修改安装目录，建议为 true，是否允许用户改变安装目录，默认是不允许
-        },
-      },
-    },
+          allowToChangeInstallationDirectory: true // 允许修改安装目录，建议为 true，是否允许用户改变安装目录，默认是不允许
+        }
+      }
+    }
   },
   css: {
     loaderOptions: {
@@ -52,14 +53,14 @@ module.exports = {
             //'border-radius-base': '4px'
           },
           // DO NOT REMOVE THIS LINE
-          javascriptEnabled: true,
+          javascriptEnabled: true
         },
-        additionalData: `@import "@/styles/theme/index.less";`,
-      },
-    },
+        additionalData: `@import "@/styles/theme/index.less";`
+      }
+    }
   },
   configureWebpack: {
-    plugins: [createThemeColorReplacerPlugin()],
+    plugins: [createThemeColorReplacerPlugin()]
   },
-  assetsDir: "static",
+  assetsDir: "static"
 };
